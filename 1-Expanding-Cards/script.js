f388c5c30eda4d02d6db6560d2d8bd00e2306739
Deepak <0822deepakverma@gmail.com>
2022-08-23 01:12:46 +0530

    Update script.js

--- 1-Expanding-Cards/script.js
+++ 1-Expanding-Cards/script.js
@@ -1,18 +1,19 @@
 // It will return you all the div(in the form of array) that have a class panel
-const panels = document.querySelectorAll('.panel');
+const panels =
+    document.querySelectorAll('.panel');
 // we are using an arrow function here
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActiveClasses();
         // With click it will add class "active" to the clicked panel
         panel.classList.add('active');
-    })
-})
+    });
+});
 
 // function to remove the class "active" from all the panels
 function removeActiveClasses() {
     // we are using the arrow function similar to line 5, but as there is only one parameter we can remove the parenthesis around "panel"
-    panels.forEach(panel => {
+    panels.forEach((panel) => {
         panel.classList.remove('active');
-    })
+    });
 }
