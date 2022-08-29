aba42c4ac107a375a293c41e275c8a1da187b816
Deepak Verma <0822deepakverma@gmail.com>
2022-08-29 10:09:09 +0530

    Update script.js

--- 7-Split-LandingPage/script.js
+++ 7-Split-LandingPage/script.js
@@ -1,6 +1,8 @@
 const leftSide = document.querySelector('.left');
 const rightSide = document.querySelector('.right');
 const container = document.querySelector('.container');
+
+// adding and removing 'hover-left' class on mouseenter and mouseleave event to the left side div
 leftSide.addEventListener('mouseenter', () => {
     container.classList.add('hover-left');
 });
@@ -8,7 +10,7 @@ leftSide.addEventListener('mouseleave', () => {
     container.classList.remove('hover-left');
 })
 
-
+// adding and removing 'hover-left' class on mouseenter and mouseleave event to the right side div
 rightSide.addEventListener('mouseenter', () => {
     container.classList.add('hover-right');
 });
