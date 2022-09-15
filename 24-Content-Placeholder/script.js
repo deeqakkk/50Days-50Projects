08261ef4b80bc55936d53309e1b54745764a1746
Deepak Verma <0822deepakverma@gmail.com>
2022-09-15 15:00:50 +0530

    Update script.js

--- 24-Content-Placeholder/script.js
+++ 24-Content-Placeholder/script.js
@@ -4,12 +4,13 @@ const excerpt = document.getElementById('excerpt')
 const profile_img = document.getElementById('profile_img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
-
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
+// Invoking the getData() function with a delay of 2000ms
 setTimeout(getData, 2000)
 
+// Function have the simplest functionality i.e to update the HTML empty content with the actual content
 function getData() {
     header.innerHTML =
         '<img src="https://cdn.pixabay.com/photo/2022/09/08/02/30/purple-flower-7439907_960_720.jpg" alt="flower" />'
